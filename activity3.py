98623f7ae980763c08553985654e33e4314ce960
Bub-Rain <levrain0714@gmail.com>
2026-09-02 16:30:53 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,35 @@
+#int
+age = 19
+height = 173
+weight = 70
+
+print("my age is", age)
+print("my height is", height)
+print("my weight is", weight)
+
+#float
+temperature = 37.5
+price = 107.5
+height = 5.8
+
+print("my temperature is", temperature)
+print("the item price is", price)
+print("my height is", height)
+
+#name
+mother = "Jendy"
+father = "Adrian"
+guardian = "Aleunamme"
+
+print("my mother is", mother)
+print("my father is", father)
+print("my guardian is", guardian)
+
+#boolean
+cool = False
+uncool = True
+im_him= False
+
+print("im cool", cool)
+print("im uncool", uncool)
+print("im him", im_him)
