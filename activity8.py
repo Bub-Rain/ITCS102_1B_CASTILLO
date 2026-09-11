05977e7ab2514cf08e6649ec49677eb5be5e5a26
Bub-Rain <levrain0714@gmail.com>
2026-09-11 14:26:16 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,35 @@
+# STRING ASSIGNMENT OPERATOR
+
+hobbies = ""
+
+a = input("What are your hobbies? (One at a time) ")
+hobbies += a + ", "
+
+b = input("What else? ")
+hobbies += b + ", "
+
+c = input("Anything else? ")
+hobbies += c + ", "
+
+d = input("Anything else? ")
+hobbies += d + ", "
+
+e = input("Anything else? ")
+hobbies += e + ", "
+
+f = input("Anything else? ")
+hobbies += f + ", "
+
+g = input("Anything else? ")
+hobbies += g + ", "
+
+h = input("Anything else? ")
+hobbies += h + ", "
+
+i = input("Anything else? ")
+hobbies += i + ", "
+
+j = input("Anything else? ")
+hobbies += j + "."
+
+print("Your hobbies are: {", hobbies, "}")
